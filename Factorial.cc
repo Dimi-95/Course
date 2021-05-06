1831c333756fe9d645d713231b9591a5b47a44c2
Dimi <46501122+DimisCode@users.noreply.github.com>
2021-05-06 13:51:04 +0200

    Add files via upload

---
+++ Factorial.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+
+unsigned long long factorial(unsigned int n)
+{
+    if (n > 1)
+    {
+        return n * factorial(n-1);
+    }
+    else
+    {
+        return 1;
+    }
+
+}
+
+int main()
+{
+
+
+    unsigned int n = 5;
+    unsigned long long n_fac = factorial(n);
+
+    std::cout << "n! = " << n_fac << std::endl;
+
+    return 0;
+}
