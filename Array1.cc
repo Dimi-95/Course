6f53c7505fc3b42a0f9f6631233cad4a8911a7bc
Dimi <46501122+DimisCode@users.noreply.github.com>
2021-05-07 09:52:11 +0200

    Add files via upload

---
+++ Array1.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    double user_balances[10] = {};
+
+    for(int i = 0; i < 10; i++)
+    {
+        cout << user_balances[i] << endl;
+    }
+
+
+    return 0;
+}
