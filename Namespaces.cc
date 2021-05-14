7fc823ab72d7664f3ca73257e3ab1d79bb340217
Dimi <46501122+DimisCode@users.noreply.github.com>
2021-05-14 10:04:30 +0200

    Add files via upload

---
+++ Namespaces.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+namespace Kino
+{
+    struct Film
+    {
+        const int jahr;
+        const char *title;
+        const char *regisseur;
+
+    };
+}
+
+
+int main()
+{
+    Kino::Film *star_wars_7 = new Kino::Film{2015, "Star Wars the Force Awakens", "J. J. Abrams" };
+
+    std::cout << star_wars_7->jahr << std::endl;
+    std::cout << star_wars_7->regisseur << std::endl;
+    std::cout << star_wars_7->title << std::endl;
+
+    Kino::Film star_wars_8{2017, "Star Wars the Last Jedi", "Rian Johnson" };
+
+    std::cout << star_wars_8.jahr << std::endl;
+    std::cout << star_wars_8.regisseur << std::endl;
+    std::cout << star_wars_8.title << std::endl;
+
+    Kino::Film star_wars_9{2019, "Star Wars the Rise of Skywalker", "J. J. Abrams" };
+
+    std::cout << star_wars_9.jahr << std::endl;
+    std::cout << star_wars_9.regisseur << std::endl;
+    std::cout << star_wars_9.title << std::endl;
+
+    return 0;
+}
