dcacda1ac08d88f9c2798f3c6e33719db7d6f47e
Dimi <46501122+DimisCode@users.noreply.github.com>
2021-05-18 15:58:23 +0200

    Add files via upload

---
+++ MathFunctions.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+
+using std::cout;
+using std::endl;
+
+void fill_double_vector(std::vector<double> &vec)
+{
+    for(int i = 0; i <  vec.size(); ++i)
+    {
+         vec[i] = i;
+    }
+}
+
+
+void print_double_vector(const std::vector<double> &vec)
+{
+    for(int i = 0; i < vec.size(); ++i)
+    {
+    cout << vec[i] << endl;
+    }
+}
+
+void double_vector_exp(std::vector<double> &vec)
+{
+    for(int i = 0; vec.size(); ++i)
+    {
+        vec[i] = std::exp(vec[i]);
+    }
+
+}
+
+void double_vector_log(std::vector<double> &vec)
+{
+    for(int i = 0; vec.size(); ++i)
+    {
+        vec[i] = std::log(vec[i]);
+    }
+
+}
+
+int main()
+{
+
+    std::vector<double> my_vector(4, 1);
+
+    fill_double_vector(my_vector);
+    print_double_vector(my_vector);
+
+    double_vector_exp(my_vector);
+    double_vector_log(my_vector);
+
+
+
+
+
+    return 0;
+}
