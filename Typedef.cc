8b35dda9e30ea3aaf600aa4ce25cfbd9d30b3c9f
Dimi <46501122+DimisCode@users.noreply.github.com>
2021-05-19 14:23:41 +0200

    Add files via upload

---
+++ Typedef.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+
+
+//typedef TYPE                           ALIAS NAME
+typedef std::vector<int>                 IntVector;
+typedef std::vector<std::vector<double>> DoubleMatrix;
+
+
+//using Alias Name = Type
+using IntMatrix = std::vector<std::vector<double>>;
+
+int main()
+{
+    IntVector my_vector;
+    DoubleMatrix my_vector2;
+    IntMatrix my_matrix2;
+
+    return 0;
+}
